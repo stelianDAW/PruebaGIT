f1af865d59fcf6d29ce5085d126b1a108d121122
GS <GS@GS-PC>
2014-04-30 19:41:13 +0200

    Mi primer committ

---
+++ src/com/iescomercio/pruebagit/stelian/ClaseStelian.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iescomercio.pruebagit.stelian;
+
+/**
+ *
+ * @author SOBREMESA1
+ */
+public class ClaseStelian {
+
+    public static void main(String args[]) {
+        System.out.println("Hola Mundo");
+    }
+}
